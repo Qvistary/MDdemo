d22649a50c9a5b6fafc334548f3e6692ad912147
qvistary <qvistary@gmail.com>
2019-03-19 09:41:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mddemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mddemo;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu_24);
+        }
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "You clicked delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.upload:
+                Toast.makeText(this, "You clicked upload", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.more:
+                Toast.makeText(this, "You clicked more", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
+
