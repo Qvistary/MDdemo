b5a904d80bc0aa9d94cd55c8d0a21a4e6b2133fb
qvistary <qvistary@gmail.com>
2019-03-22 11:33:21 +0800

    Change nav

--- app/src/main/java/com/example/mddemo/MainActivity.java
+++ app/src/main/java/com/example/mddemo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mddemo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -20,11 +23,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.baseline_menu_24);
         }
+
+        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_call:
+                        Log.d("TAG", "You click NAV_CALL");
+                        break;
+                    case R.id.nav_setting:
+                        Log.d("TAG", "You click NAV_SETTING");
+                        break;
+                    case R.id.nav_home:
+                        Log.d("TAG", "You click NAV_HOME");
+                        break;
+                }
+
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
 
     }
 
